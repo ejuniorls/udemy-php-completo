e326a9f59cee50a1b49956b30edce6a822a38519
edmir.lobo <ejuniorls@outlook.com>
2023-05-01 21:19:45 -0300

    Seção 3: Integrando PHP com HTML & CSS

--- index.php
+++ index.php
@@ -10,24 +10,44 @@
         <h1>Curso PHP</h1>
         <h2>Índice dos Exercícios</h2>
     </header>
-
     <main class="principal">
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01 - Básico</h3>
+                    <h3>Básico</h3>
                     <ul>
-                        <li><a href="exercicio.php">Exercício A</a></li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=ola">
+                                Olá PHP
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=html">
+                                Integração HTML
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=css">
+                                Integração CSS
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">
+                                Comentários PHP
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">
+                                Desafio
+                            </a>
+                        </li>
                     </ul>
                 </div>
-
             </nav>
-
         </div>
     </main>
-
     <footer class="rodape">
-        COD3R & ALUNOS &copy <?= date('Y') ?>
+        COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 </body>
 </html>
